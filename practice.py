afe0330b089b563b94330d09e9f706a7c031696d
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 15:04:04 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,25 @@
-print("hello")
-print("world")
-print("hello")
+def observe_birds(observations_file: TextIO) -> Set[str]:
+    """ Return a set of the bird species listed in observations_file, which has
+    one bird species per line.
+    
+    >>> infile = StringIO('bird 1\\nbird 2\\nbird 1\\n')
+    >>> birds = observe_birds(infile)
+    >>> 'birds 1' in birds
+    True
+    >>> 'bird 2' in birds 
+    True
+    >>> len(birds) == 2
+    True
+    """
+    birds_observed = set()
+    for line in observations_file:
+        bird = line.strip()
+        birds_observed.add(bird)
+        
+    return birds_observed
+
+  if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    with open('observations.txt') as observations_file:
+      print(observe_birds(observations_file))
